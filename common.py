552b350df35a0e8a13255a38b704dcc0b5d677d1
paw <pawsen@gmail.com>
2017-08-03 17:37:20 +0200

    Crude version. Seems to work... yaj:)

--- common.py
+++ common.py
@@ -36,3 +36,30 @@ def rescale(x, mini=None, maxi=None):
     if maxi is None:
         maxi = np.max(x)
     return (x - mini) / (maxi - mini)
+
+def meanVar(Y):
+    """
+    Y = fft(y)/nsper
+
+    Parameters
+    ----------
+    Y : ndarray (ndof, nsper, nper)
+        Y is the fft of y
+    """
+
+    # number of periods
+    p = Y.shape[2]
+
+    # average over periods
+    Ymean = np.sum(Y,axis=2) / p
+
+    # subtract column mean from y in a broadcast way. Ie: y is 3D matrix and
+    # for every 2D slice we subtract y_mean. Python automatically
+    # broadcast(repeat) y_mean.
+    # https://scipy.github.io/old-wiki/pages/EricsBroadcastingDoc
+    Y0 = Y - Ymean[...,None]
+
+    # weights. Only used if the signal is noisy
+    W = np.sum(np.abs(Y0)**2, axis=2)/( p-1)
+
+    return Ymean, W
